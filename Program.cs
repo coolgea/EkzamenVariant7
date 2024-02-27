ea8aae9350e8bd52902daeaeb581fa4651705c19
HOLYSHIELD <157357160+BLVCKDIAMONDSKY@users.noreply.github.com>
2024-02-27 12:07:41 +0300

    Задание готово к проверке

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         Console.WriteLine("Enter X");
         double x = Convert.ToDouble(Console.ReadLine());
         double y = Math.Cos(x);
+        y = y * 2;
         Console.WriteLine("Result = " + y);
+
     }
 }
