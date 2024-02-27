8053f002b11dc836afb4f06328a634cc71f8f26a
HOLYSHIELD <157357160+BLVCKDIAMONDSKY@users.noreply.github.com>
2024-02-27 12:03:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter X");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Math.Cos(x);
+        Console.WriteLine("Result = " + y);
+    }
+}
